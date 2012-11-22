9fcf6390ad24d411ec7cee998e6e6ed36de0638f
Sahil Jain <sdj09@ic.ac.uk>
2012-11-22 18:15:48 +0000

    Made the basic webpage generation classes. untested

--- src/crowdtrust/LoginServlet.java
+++ src/crowdtrust/LoginServlet.java
@@ -6,17 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.ServletException;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.PreparedStatement;
+import db.LoginDb;
 
 import java.util.Properties;
 
 import java.io.IOException;
 
 import java.security.MessageDigest;
+import web.*;
 
 public class LoginServlet extends HttpServlet {
 
@@ -29,34 +26,16 @@ public class LoginServlet extends HttpServlet {
     }  
     String username = request.getParameter("username");
     String password = request.getParameter("password");
-    try {
-      String url = "jdbc:postgresql://db:5432/g1236218_u";
-      Properties properties = new Properties();
-      properties.setProperty("user", "g1236218_u");
-      properties.setProperty("password", "RLTn4ViKks");
-      connection = DriverManager.getConnection(url, properties);
-      if (connection != null) {
-        String sql = "SELECT id FROM accounts WHERE username = ? AND password = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1, username);
-        preparedStatement.setBytes(2, sha256(password));
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if(!resultSet.next() || !resultSet.isLast()) {
-          //user does not exist - display error message
-          return;
-        }
-        if(!request.isRequestedSessionIdValid()) {
-          HttpSession session = request.getSession();
-          session.setMaxInactiveInterval(1200);
-          session.setAttribute("account_id", resultSet.getInt("id"));
-        }
-      }
+    int id = LoginDb.checkUserDetails(username, password);
+    if(!request.isRequestedSessionIdValid() && id > 0) {
+      HttpSession session = request.getSession();
+      session.setMaxInactiveInterval(1200);
+      session.setAttribute("account_id", resultSet.getInt("id"));
     }
-    catch (SQLException e) {
-      throw new ServletException(e);
-    }
-    response.setHeader("Location", "/lobby.html");
-    //response.sendRedirect("/lobby.html");
+    Lobby userLobby = new Lobby();
+    userLobby.addClientTable();
+    PrintWriter out = response.getWriter();
+    out.print(userLobby.generate());
   }
 
   private byte[] sha256(String password) {
