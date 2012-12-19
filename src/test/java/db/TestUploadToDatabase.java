cd8c6eba76033f254551b66e4bcb0dfff49e648e
Adam <af1410@ic.ac.uk>
2012-12-19 21:14:43 +0000

    beginning test writing

---
+++ src/test/java/db/TestUploadToDatabase.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import junit.framework.TestCase;
+
+public class TestUploadToDatabase extends TestCase{
+
+	public TestUploadToDatabase(String name) {
+		super(name);
+	}
+	
+	public void testUpload() throws Exception {
+		String commandTomcatStart = "tomcat6 start";
+		String commandTomcatStop = "tomcat6 stop";
+		//Process tomcatServer = Runtime.getRuntime().exec(commandTomcatStart);
+		//InputStream is = tomcatServer.getInputStream();
+		//String result = IOUtils.toString(is, "ASCII");
+		//Process tomcatStop = Runtime.getRuntime().exec(commandTomcatStop);
+		assertTrue(true);
+	}
+
+}
