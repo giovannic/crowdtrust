19db1b2b3510ce3f3be20058b9f1ad363fa082a8
adam <af1410@ic.ac.uk>
2012-12-22 03:52:18 +0000

    db test should initialise db before running (should take this out into separate file)

--- src/test/java/servlet/TestRegisterServlet.java
+++ src/test/java/servlet/TestRegisterServlet.java
@@ -1,15 +1,24 @@
 package servlet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+
+import db.DbAdaptor;
 
 public class TestRegisterServlet extends TestCase {
 
@@ -22,11 +31,33 @@ public class TestRegisterServlet extends TestCase {
 	private static final String CLIENT = "testcl";
 	private static final String CROWD = "testcr";
 	
+	private final static String URL = "jdbc:postgresql://db:5432/g1236218_u";
+	private final static String USER = "g1236218_u";
+	private final static String PASSWORD = "RLTn4ViKks";
+	private final static String INITSQL_LOC = "src/main/sql/init.sql";
 	private WebClient client;
+	
+	private Connection connection = null;
 	
 	@Before
 	public void before() {
-		client = new WebClient();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(INITSQL_LOC));
+			StringBuffer sb = new StringBuffer();
+			String sqlLine;
+			while((sqlLine = reader.readLine()) != null) {
+				sb.append(sqlLine + "\n");
+			}
+			reader.close();
+			connection = DbAdaptor.connect();
+			Statement s = null;
+			connection.createStatement().execute(sb.toString());
+		} catch (Exception e) {
+			System.err.println("SOMEBODY MOVED OR FUDGED init.sql");
+			System.err.println("currently looking for it in: " + INITSQL_LOC);
+			e.printStackTrace();
+		}
+		System.out.println("I HAVE RUN");
 	}
 	
 	public void testServlet() throws Exception{
