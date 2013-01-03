dad4646001d24d853b7b7eb7c396983799430887
Sahil Jain <sdj09@ic.ac.uk>
2013-01-03 14:15:31 +0000

    no conflicts

--- src/main/java/db/DbAdaptor.java
+++ src/main/java/db/DbAdaptor.java
@@ -12,26 +12,12 @@ public class DbAdaptor {
 	private final static String USER = "g1236218_u";
 	private final static String PASSWORD = "RLTn4ViKks";
 	
-<<<<<<< HEAD
-	public static Connection connect(){
-		try {
-	    	Properties properties = new Properties();
-	    	properties.setProperty("user", USER);
-	    	properties.setProperty("password", PASSWORD);
-	    	Connection connection = DriverManager.getConnection(URL, properties);
-			return connection;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-=======
 	public static Connection connect() throws ClassNotFoundException, SQLException{
 		Class.forName("org.postgresql.Driver");
 		Properties properties = new Properties();
 		properties.setProperty("user", USER);
 		properties.setProperty("password", PASSWORD);
 		return DriverManager.getConnection(URL, properties);
->>>>>>> 442f6939a38faa10af5a3e2d8eb32de230227dda
 	}
 	
 }
