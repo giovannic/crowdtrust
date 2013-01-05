d42aae44ad0caead1fc31c9bc3041fa6708a1ffd
Sahil Jain <sdj09@ic.ac.uk>
2013-01-05 23:40:53 +0000

    compiles

--- src/test/java/algorithm/TestAlgorithm.java
+++ src/test/java/algorithm/TestAlgorithm.java
@@ -78,10 +78,10 @@ public class TestAlgorithm extends TestCase {
 			uuid = uuid.substring(0, 12);
 			SubTaskDb.addSubtask(uuid, TaskDb.getTaskId("BinaryTestTask"));
 		//	int id = SubTaskDb.getSubTaskId(uuid);
-			System.out.println("Subtask Id: " + id);
-			BinarySubTask bst = new BinarySubTask(id);
-			AnnotatorSubTaskAnswer asta = new AnnotatorSubTaskAnswer(bst.getId(), bst, new BinaryTestData(rand.nextInt(1)));
-			answers.add(asta);
+		//	System.out.println("Subtask Id: " + id);
+		//	BinarySubTask bst = new BinarySubTask(id);
+		//	AnnotatorSubTaskAnswer asta = new AnnotatorSubTaskAnswer(bst.getId(), bst, new BinaryTestData(rand.nextInt(1)));
+		//	answers.add(asta);
 		}
 		
 		//Give all the annotators the answers
