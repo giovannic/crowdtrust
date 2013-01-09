9f5d9d46493e60b96d4af1c587e59aa6dbd6ecab
John <j-p-walker@hotmail.co.uk>
2013-01-09 03:11:43 +0000

    fixing with gio

--- src/main/java/db/SubTaskDb.java
+++ src/main/java/db/SubTaskDb.java
@@ -440,6 +440,7 @@ public class SubTaskDb {
 	private static SubTask mapSubTask(ResultSet rs, int type) throws SQLException {
 		SubTask s = null;
 		int taskAccuracy = rs.getInt("acc");
+		System.out.println("Task acc: " + taskAccuracy);
 		int id = rs.getInt("sid");
 		int responses = rs.getInt("c");
 		int maxLabels = rs.getInt("ml");
