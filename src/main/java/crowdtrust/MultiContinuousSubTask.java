6742ff2cf79246f18be74e9507108bc28f1d3b48
Giovanni <giovanni.charles@gmail.com>
2013-01-15 18:31:57 +0000

    continuous integer division

--- src/main/java/crowdtrust/MultiContinuousSubTask.java
+++ src/main/java/crowdtrust/MultiContinuousSubTask.java
@@ -22,7 +22,7 @@ public class MultiContinuousSubTask extends ContinuousSubTask {
 		ContinuousResponse cz = (ContinuousResponse) z;
 		
 		int total = a.getN() + 2;
-		double w = total/(total + 1);
+		double w = (double) total/(total + 1);
 		double alpha = sa.getAccuracy()*total;
 		
 		MultiGaussianDistribution mgd =
